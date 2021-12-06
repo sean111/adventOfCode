70e625f8165444513bb2a1629c1abd70d44d3b04
Sean Burke <sean111@gmail.com>
2021-12-06 16:45:22 -0500

    Day 5 part 2 complete

--- 2021/day5/day5.go
+++ 2021/day5/day5.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	var vents [1000][1000]int
 	var overlaps = 0
-	data, err := os.Open("test.txt")
+	data, err := os.Open("data.txt")
 	if err != nil {
 		log.Fatalln(err)
 		return
@@ -33,35 +33,33 @@ func main() {
 		y1, _ := strconv.Atoi(coords[1])
 		x2, _ := strconv.Atoi(coords[2])
 		y2, _ := strconv.Atoi(coords[3])
-		start := 0
-		end := 0
-		if x1 == x2 {
-			if y1 > y2 {
-				start = y2
-				end = y1
-			} else {
-				start = y1
-				end = y2
-			}
-			for i := start; i <= end; i++ {
-				vents[x1][i]++
-				if vents[x1][i] == 2 {
-					overlaps++
+
+		vents[x1][y1]++
+		if vents[x1][y1] == 2 {
+			overlaps++
+		}
+		for {
+			if x1 != x2 {
+				if x1 > x2 {
+					x1--
+				} else {
+					x1++
 				}
 			}
-		} else if y1 == y2 {
-			if x1 > x2 {
-				start = x2
-				end = x1
-			} else {
-				start = x1
-				end = x2
-			}
-			for i := start; i <= end; i++ {
-				vents[i][y1]++
-				if vents[i][y1] == 2 {
-					overlaps++
+			if y1 != y2 {
+				if y1 > y2 {
+					y1--
+				} else {
+					y1++
 				}
+			}
+			vents[x1][y1]++
+			if vents[x1][y1] == 2 {
+				overlaps++
+			}
+
+			if x1 == x2 && y1 == y2 {
+				break
 			}
 		}
 	}
