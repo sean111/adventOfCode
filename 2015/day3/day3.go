5869bba6d302a8133fa40fe5b7e27064dfd7aa25
Sean Burke <seanb@eshark.net>
2022-08-11 03:20:47 +0000

    Answer for 2015/day3

--- 2015/day3/day3.go
+++ 2015/day3/day3.go
@@ -6,17 +6,28 @@ import (
 	"os"
 )
 
+var houses int
+var houseMap = map[int]map[int]int{}
+
+type Coords struct {
+	x int
+	y int
+}
+
+var coords Coords
+
 func main() {
-	data, err := os.Open("test.txt")
+	data, err := os.Open("data.txt")
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	var houseMap [][]int
-	houses := 1
-
-	houseMap[0][0] = 1 //Initial delivery
+	houses = 1
+	houseMap[0] = map[int]int{
+		0: 1,
+	} //Initial delivery
+	coords = Coords{x: 0, y: 0}
 
 	reader := bufio.NewReader(data)
 	for {
@@ -29,6 +40,38 @@ func main() {
 			}
 		}
 
-		log.Printf("%s", string(direction))
+		// log.Printf("%s", string(direction))
+		switch string(direction) {
+		case ">":
+			move(coords.x+1, coords.y)
+		case "<":
+			move(coords.x-1, coords.y)
+		case "^":
+			move(coords.x, coords.y+1)
+		case "v":
+			move(coords.x, coords.y-1)
+		}
 	}
+	log.Printf("Houses: %d", houses)
+}
+
+func move(x int, y int) {
+	_, ok := houseMap[x][y]
+	if !ok {
+		if len(houseMap[x]) > 0 {
+			houseMap[x][y] = 1
+		} else {
+			houseMap[x] = map[int]int{
+				y: 1,
+			}
+		}
+		houses++
+	} else {
+		houseMap[x][y]++
+	}
+	// log.Printf("%v\n", ok)
+	// log.Printf("x: %d, y: %d, testVal: %d\n", x, y, testVal)
+	// log.Printf("map: %v\n", houseMap)
+	coords.x = x
+	coords.y = y
 }
