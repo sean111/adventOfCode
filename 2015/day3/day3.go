5159adaa4739f76ba36eada30b0745117188a0a1
Sean Burke <seanb@eshark.net>
2022-08-11 01:17:22 +0000

    More setup for 2015/day3

--- 2015/day3/day3.go
+++ 2015/day3/day3.go
@@ -1,1 +1,34 @@
-package day3
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var houseMap [][]int
+	houses := 1
+
+	houseMap[0][0] = 1 //Initial delivery
+
+	reader := bufio.NewReader(data)
+	for {
+		direction, err := reader.ReadByte()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			} else {
+				log.Fatalln(err)
+			}
+		}
+
+		log.Printf("%s", string(direction))
+	}
+}
