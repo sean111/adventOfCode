37cfd216da3843c467b978879c7bca47d90c8e79
Sean Burke <sean111@gmail.com>
2022-12-04 20:12:24 -0500

    Completed 2022.3.2

--- 2022/day3/day3.go
+++ 2022/day3/day3.go
@@ -11,6 +11,21 @@ type Rucksack struct {
 	Compartment2 []rune
 }
 
+func (sack Rucksack) Combined() []rune {
+	return append(sack.Compartment1, sack.Compartment2...)
+}
+
+func (sack Rucksack) Contains(item rune) bool {
+	//Check compartment 1
+	sackItems := sack.Combined()
+	for _, sackItem := range sackItems {
+		if sackItem == item {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	data, err := os.Open("data.txt")
 	if err != nil {
@@ -20,14 +35,31 @@ func main() {
 	scanner := bufio.NewScanner(data)
 
 	var total int
+	var p2Total int
+	groupCount := 0
+	group := make([]Rucksack, 0)
 	for scanner.Scan() {
 		sack := ParseContainer(scanner.Text())
 		dupes := FindDuplicates(sack)
 		for _, dupe := range dupes {
 			total += GetItemPriority(dupe)
 		}
+		groupCount++
+		if groupCount == 3 {
+			// Do group check logic
+			commonItem := CheckGroupForCommonItem(append(group, sack))
+			if commonItem != rune(0) {
+				p2Total += GetItemPriority(commonItem)
+			}
+			groupCount = 0
+			group = make([]Rucksack, 0)
+		} else {
+			group = append(group, sack)
+		}
 	}
-	fmt.Printf("Total: %d\n", total)
+	// Check the end of the group array
+	fmt.Printf("P1 Total: %d\n", total)
+	fmt.Printf("P2 Total: %d\n", p2Total)
 }
 
 func ParseContainer(items string) Rucksack {
@@ -58,6 +90,16 @@ func GetItemPriority(item rune) int {
 		modifier = 38
 	}
 	return tmp - modifier
+}
+
+func CheckGroupForCommonItem(sacks []Rucksack) rune {
+	firstSack := sacks[0].Combined()
+	for _, item := range firstSack {
+		if sacks[1].Contains(item) && sacks[2].Contains(item) {
+			return item
+		}
+	}
+	return rune(0)
 }
 
 func containsRune(list []rune, target rune) bool {
