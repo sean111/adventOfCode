639c840a37882ed2bd42e65bf4d2edaaf66c0296
Sean Burke <sean111@gmail.com>
2022-12-06 03:56:06 +0000

    Completed 2022.4.1

--- 2022/day4/day4.go
+++ 2022/day4/day4.go
@@ -25,27 +25,44 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer data.Close()
+
+	writeFile, err := os.OpenFile("output.txt", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+	defer writeFile.Close()
 
 	scanner := bufio.NewScanner(data)
 	var total int
 	for scanner.Scan() {
 		assignments := ParseInput(scanner.Text())
 		if assignments[0].Contains(assignments[1]) || assignments[1].Contains(assignments[0]) {
+			_, err := writeFile.WriteString(fmt.Sprintf("%d - %d, %d - %d || [%s]\n", assignments[0].start, assignments[0].end, assignments[1].start, assignments[1].end, scanner.Text()))
+			if err != nil {
+				panic(err)
+			}
 			total++
 		}
 	}
+	writeFile.Sync()
 	fmt.Printf("Total: %d\n", total)
 }
 
 func ParseInput(input string) []Assignment {
-	r := regexp.MustCompile(`(\d)-(\d)`)
+	r := regexp.MustCompile(`(\d+)-(\d+)`)
 	match := r.FindAllStringSubmatch(input, -1)
 	assignments := make([]Assignment, 0)
 	for _, m := range match {
-		startPoint, _ := strconv.Atoi(m[1])
-		endPoint, _ := strconv.Atoi(m[2])
-		assignments = append(assignments, Assignment{start: startPoint, end: endPoint})
+		firstPoint, _ := strconv.Atoi(m[1])
+		secondPoint, _ := strconv.Atoi(m[2])
 
+		if firstPoint > secondPoint {
+			assignments = append(assignments, Assignment{start: secondPoint, end: firstPoint})
+		} else {
+			assignments = append(assignments, Assignment{start: firstPoint, end: secondPoint})
+		}
 	}
 	return assignments
 }
--- 2022/day4/day4_test.go
+++ 2022/day4/day4_test.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAssignmentContains(t *testing.T) {
+	var tests = []struct {
+		assignment1 Assignment
+		assignment2 Assignment
+		result      bool
+	}{
+		{assignment1: Assignment{start: 1, end: 3}, assignment2: Assignment{start: 2, end: 3}, result: true},
+		{assignment1: Assignment{start: 6, end: 9}, assignment2: Assignment{start: 1, end: 7}, result: false},
+		{assignment1: Assignment{start: 10, end: 11}, assignment2: Assignment{start: 9, end: 22}, result: true},
+		{assignment1: Assignment{start: 13, end: 11}, assignment2: Assignment{start: 10, end: 12}, result: true},
+		{assignment1: Assignment{start: 18, end: 12}, assignment2: Assignment{start: 13, end: 11}, result: false},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v, %v", test.assignment1, test.assignment2), func(t *testing.T) {
+			ans := false
+
+			if test.assignment1.Contains(test.assignment2) || test.assignment2.Contains(test.assignment1) {
+				ans = true
+			}
+
+			if ans != test.result {
+				t.Errorf("got %t, want %t", ans, test.result)
+			}
+		})
+	}
+}
+
+func TestParseInput(t *testing.T) {
+	var tests = []struct {
+		input       string
+		assignment1 Assignment
+		assignment2 Assignment
+	}{
+		{
+			input:       "17-72,16-71",
+			assignment1: Assignment{start: 17, end: 72},
+			assignment2: Assignment{start: 16, end: 71},
+		},
+		{
+			input:       "3-31,1-32",
+			assignment1: Assignment{start: 3, end: 31},
+			assignment2: Assignment{start: 1, end: 32},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			ans := ParseInput(test.input)
+			if ans[0].start != test.assignment1.start || ans[0].end != test.assignment1.end || ans[1].start != test.assignment2.start || ans[1].end != test.assignment2.end {
+				t.Errorf("got %v, %v, want %v, %v", ans[0], ans[1], test.assignment1, test.assignment2)
+			}
+		})
+	}
+}
