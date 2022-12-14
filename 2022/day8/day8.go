6e1e2ce7cc4c5fe91b60bc49b3ab1b09ea04c3f8
Sean Burke <sean111@gmail.com>
2022-12-14 08:59:27 -0500

    Completed 2022.8.2

--- 2022/day8/day8.go
+++ 2022/day8/day8.go
@@ -4,16 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/fatih/color"
-	"log"
 	"os"
 	"strconv"
 )
 
 type Tree struct {
-	height  int
-	visible bool
-	checked bool
+	height      int
+	visible     bool
+	visibleFrom rune // For debugging
+	score       int
 }
+
+var MaxScore int
 
 var Map [][]Tree
 
@@ -34,13 +36,10 @@ func main() {
 			tmp, _ := strconv.Atoi(string(c))
 			//fmt.Printf("Row: %d, Column: %d, Value: %d,\n", row, column, tmp)
 			vis := false
-			chkd := false
 			if row == 0 || column == 0 || column == len(line)-1 {
 				vis = true
-				chkd = true
 			}
-			//Map[row][column] = Tree{height: tmp, visible: vis, checked: false}
-			tmpRow = append(tmpRow, Tree{height: tmp, visible: vis, checked: chkd})
+			tmpRow = append(tmpRow, Tree{height: tmp, visible: vis, visibleFrom: ' '})
 		}
 		Map = append(Map, tmpRow)
 		//column = 0
@@ -49,102 +48,120 @@ func main() {
 
 	//Go back through and set last row to visible
 	for column := 1; column < len(Map[row-1])-1; column++ {
-		Map[row-1][column].visible = true
-		Map[row-1][column].checked = true
+		Tree := &Map[row-1][column]
+		Tree.visible = true
+	}
+	for r := 0; r <= len(Map)-1; r++ {
+		for c := 0; c <= len(Map[1])-1; c++ {
+			CheckTree(r, c)
+		}
+	}
+	// CheckTree(4, 3)
+	//fmt.Println("Final Map")
+	//ShowMap()
+
+	vTrees := 0
+	for r := 0; r <= len(Map)-1; r++ {
+		for c := 0; c <= len(Map[r])-1; c++ {
+			if Map[r][c].visible == true {
+				vTrees++
+			}
+		}
 	}
 
-	fmt.Println("Initial Map")
-	ShowMap()
-	IsTreeVisible(1, 1)
-	fmt.Println("Final Map")
-	ShowMap()
+	fmt.Printf("Real Visible Trees: %d\n", vTrees)
+	fmt.Printf("Max Score: %d\n", MaxScore)
 }
 
-func IsTreeVisible(row int, column int) bool {
+func CheckTree(row int, column int) {
+	//log.Println()
+	var foundTallerTree bool
+	var tmpScore int
 	tree := &Map[row][column]
-	log.Printf("Tree: %v\n", tree)
 
-	var lVisible bool = false
-	var rVisible bool = false
-	var uVisible bool = false
-	var dVisible bool = false
+	// Check Up
+	foundTallerTree = false
+	tmpScore = 0
+	for i := row - 1; i >= 0; i-- {
+		tmpScore++
+		target := Map[i][column]
+		if target.height >= tree.height {
+			foundTallerTree = true
+			break
+		}
+	}
+	if !foundTallerTree {
+		tree.visibleFrom = 'U'
+		tree.visible = true
+	}
+	tree.score += tmpScore
+
+	// Check Right
+	foundTallerTree = false
+	tmpScore = 0
+	for i := column + 1; i < len(Map[row]); i++ {
+		//log.Printf("- %d, %d ::", row, i)
+		tmpScore++
+		target := Map[row][i]
+		if target.height >= tree.height {
+			foundTallerTree = true
+			break
+		}
+	}
+	if !foundTallerTree {
+		tree.visibleFrom = 'R'
+		tree.visible = true
+	}
+	tree.score *= tmpScore
 
 	// Check Left
-	if column > 0 {
-		lVisible = Map[row][column-1].visible
-		lHeight := Map[row][column-1].height
-		if Map[row][column-1].checked != true {
-			lVisible = IsTreeVisible(row, column-1)
+	foundTallerTree = false
+	tmpScore = 0
+	for i := column - 1; i >= 0; i-- {
+		tmpScore++
+		target := Map[row][i]
+		if target.height >= tree.height {
+			foundTallerTree = true
+			break
 		}
-
-		if lVisible == true && lHeight < tree.height {
-			Map[row][column].visible = true
-			tree.visible = true
-		}
-		Map[row][column].checked = true
 	}
-
-	// Check right
-	if column < len(Map[row]) {
-		rVisible = Map[row][column+1].visible
-		rHeight := Map[row][column+1].height
-
-		if Map[row][column+1].checked != true {
-			rVisible = IsTreeVisible(row, column+1)
-		}
-
-		if rVisible == true && rHeight < tree.height {
-			Map[row][column].visible = true
-		}
-		Map[row][column].checked = true
+	if !foundTallerTree {
+		tree.visibleFrom = 'L'
+		tree.visible = true
 	}
+	tree.score *= tmpScore
 
-	//Check Up
-	if row > 0 {
-		uVisible = Map[row-1][column].visible
-		uHeight := Map[row-1][column].height
-
-		if Map[row-1][column].checked != true {
-			uVisible = IsTreeVisible(row-1, column)
+	// Check Down
+	foundTallerTree = false
+	tmpScore = 0
+	for i := row + 1; i < len(Map); i++ {
+		tmpScore++
+		target := Map[i][column]
+		if target.height >= tree.height {
+			foundTallerTree = true
+			break
 		}
-
-		if uVisible == true && uHeight < tree.height {
-			Map[row][column].visible = true
-		}
-		Map[row][column].checked = true
 	}
-
-	if row < len(Map) {
-		dVisible = Map[row+1][column].visible
-		dHeight := Map[row+1][column].height
-
-		if Map[row+1][column].checked != true {
-			dVisible = IsTreeVisible(row+1, column)
-		}
-
-		if dVisible == true && dHeight < tree.height {
-			Map[row][column].visible = true
-		}
-		Map[row][column].checked = true
+	if !foundTallerTree {
+		tree.visibleFrom = 'D'
+		tree.visible = true
 	}
-
-	if lVisible || rVisible || uVisible || dVisible {
-		return true
+	tree.score *= tmpScore
+	if tree.score > MaxScore {
+		MaxScore = tree.score
 	}
-	return false
 }
 
 func ShowMap() {
 	for row := 0; row < len(Map); row++ {
 		for column := 0; column < len(Map[row]); column++ {
 			tree := Map[row][column]
-			//fmt.Printf("%d [%t,%t]\t", Map[row][column].height, Map[row][column].visible, Map[row][column].checked)
 			outColor := color.New(color.FgGreen).SprintfFunc()
 			if !tree.visible {
 				outColor = color.New(color.FgRed).SprintfFunc()
 			}
 			tmp := strconv.Itoa(tree.height)
-			fmt.Printf("%s\t", outColor(tmp))
+			fmt.Printf("%s[%d] ", outColor(tmp), tree.score)
 
 		}
 		fmt.Println()
