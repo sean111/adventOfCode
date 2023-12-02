f840dc8071dcc3321065b3b6fde5b8ecb646e4b2
Sean Burke <sean111@gmail.com>
2023-12-01 20:18:45 -0500

    2023-1.2 completed

--- 2023/day1/src/Day1.php
+++ 2023/day1/src/Day1.php
@@ -26,10 +26,11 @@ class Day1 {
     private function parseLine(array $line): int {
 //        $tmp = $this->getFirstInt($line) . $this->getLastInt($line);
         if (!$first = $this->getFirstInt($line)) {
-            throw new \Exception("error getting first int");
+            throw new \Exception("error getting first int (" . implode('',$line) . ")");
         }
+
         if (!$last= $this->getLastInt($line)) {
-            throw new \Exception("error getting last int");
+            throw new \Exception("error getting last int (" . implode('',$line) . ")");
         }
         $tmp = $first.$last;
 //        var_dump(['line'=> $line,'first' =>$first, 'last' =>$last, 'tmp' => $tmp, 'int' => (int)$tmp]);
@@ -41,6 +42,49 @@ class Day1 {
             if (is_numeric($line[$x])) {
                 return $line[$x];
             }
+
+            // Check for strings
+            if ($x+3 < $cnt) {
+                $tmp = implode('', array_slice($line, $x, 3));
+                switch($tmp) {
+                    case "one":
+                        return 1;
+                    case "two":
+                        return 2;
+                    case "thr":
+                        if (implode('', array_slice($line, $x, 5)) == "three") {
+                            return 3;
+                        }
+                        break;
+                    case "fou":
+                        if (implode('', array_slice($line, $x, 4)) == "four") {
+                            return 4;
+                        }
+                        break;
+                    case "fiv":
+                        if (implode('', array_slice($line, $x, 4)) == "five") {
+                            return 5;
+                        }
+                        break;
+                    case "six":
+                        return 6;
+                    case "sev":
+                        if (implode('', array_slice($line, $x, 5)) == "seven") {
+                            return 7;
+                        }
+                        break;
+                    case "eig":
+                        if (implode('', array_slice($line, $x, 5)) == "eight") {
+                            return 8;
+                        }
+                        break;
+                    case "nin":
+                        if (implode('', array_slice($line, $x, 4)) == "nine") {
+                            return 9;
+                        }
+                        break;
+                }
+            }
         }
         return false;
     }
@@ -49,6 +93,47 @@ class Day1 {
         for ($x = (count($line) - 1); $x >= 0; $x--) {
             if (is_numeric($line[$x])) {
                 return $line[$x];
+            }
+
+            //Check for strings
+            $tmp = implode('', array_slice($line, $x-3, 3));
+            switch($tmp) {
+                case "one":
+                    return 1;
+                case "two":
+                    return 2;
+                case "thr":
+                    if (implode('', array_slice($line, $x-3, 5)) == "three") {
+                        return 3;
+                    }
+                    break;
+                case "fou":
+                    if (implode('', array_slice($line, $x-3, 4)) == "four") {
+                        return 4;
+                    }
+                    break;
+                case "fiv":
+                    if (implode('', array_slice($line, $x-3, 4)) == "five") {
+                        return 5;
+                    }
+                    break;
+                case "six":
+                    return 6;
+                case "sev":
+                    if (implode('', array_slice($line, $x-3, 5)) == "seven") {
+                        return 7;
+                    }
+                    break;
+                case "eig":
+                    if (implode('', array_slice($line, $x-3, 5)) == "eight") {
+                        return 8;
+                    }
+                    break;
+                case "nin":
+                    if (implode('', array_slice($line, $x-3, 4)) == "nine") {
+                        return 9;
+                    }
+                    break;
             }
         }
         return false;
--- 2023/day1/tests/Unit/SumTest.php
+++ 2023/day1/tests/Unit/SumTest.php
@@ -5,5 +5,5 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 test('sum', function () {
     $day1 = new AoC\Day1(__DIR__ . "/../../test.txt");
     $result = $day1->sum();
-    expect($result)->toBe(142);
+    expect($result)->toBe(281);
 });
