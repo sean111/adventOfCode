f3f5513d3cea6e96208bc77b64316e75c716602a
Sean Burke <sean111@gmail.com>
2023-12-08 00:10:45 -0500

    setup for day 4

---
+++ 2023/day4.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use AoC\Y2023\Day4;
+
+test('day 4', function () {
+    $day4 = new Day4;
+    $sum = $day4->getPoints();
+   expect($sum)->toBe(13);
+});
---
+++ 2023/src/Day4.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AoC\Y2023;
+
+class Day4
+{
+    private int $totalPoints = 0;
+    public function getPoints()
+    {
+        return $this->totalPoints;
+    }
+}
