b87d16af70a7e74ecf81ef7062459cf9d270945f
Sean Burke <sean111@gmail.com>
2023-12-09 19:57:59 -0500

    Finally had time to find and fix the bug.

--- 2023/src/Day3.php
+++ 2023/src/Day3.php
@@ -16,35 +16,35 @@ class Day3
     public function addRow(string $row): void {
         $columns = str_split($row);
         $this->schematic[] = $columns;
-        if ($this->totalColumns == 0) {
-            $this->totalColumns = count($columns) -1;
-        }
-        $this->totalRows++;
     }
 
     public function findSum(): int {
+        $this->totalRows = count($this->schematic);
+        $this->totalColumns = count($this->schematic[0]);
         for ($r = 0; $r < $this->totalRows; $r++) {
             for ($c = 0; $c < $this->totalColumns; $c++) {
                 $tmp = $this->schematic[$r][$c];
                 if ($tmp == '.' || !is_numeric($tmp)) {
                     if ($this->touchesSymbol) {
                         $val = intval(implode('', $this->currentBuffer));
-                        dump("Adding $val to $this->sum");
                         $this->sum += $val;
                     }
                     $this->currentBuffer = [];
                     $this->touchesSymbol = false;
+
                 } else if (is_numeric($tmp)) {
                     if (!$this->touchesSymbol) {
-//                        print "Checking $tmp ($r, $c)" . PHP_EOL;
                         if ($this->checkForSymbol($r, $c)) {
-//                            print "\t$tmp touches a symbol" . PHP_EOL;
                             $this->touchesSymbol = true;
                         }
                     }
                     $this->currentBuffer[] = $tmp;
                 }
                 unset($tmp);
+            }
+            if ($this->touchesSymbol) {
+                $val = intval(implode('', $this->currentBuffer));
+                $this->sum += $val;
             }
             $this->currentBuffer = [];
             $this->touchesSymbol = false;
@@ -55,49 +55,41 @@ class Day3
     private function checkForSymbol(int $row, int $column): bool {
 
         // Check left
-//        print "\tLeft: " . PHP_EOL;
         if ($this->isSymbol($row, $column-1)) {
             return true;
         }
 
         // Check top left
-//        print "\tTop Left: " . PHP_EOL;
         if ($this->isSymbol($row-1, $column-1)) {
             return true;
         }
 
         // Check top
-//        print "\tTop: " . PHP_EOL;
         if ($this->isSymbol($row-1, $column)) {
             return true;
         }
 
         // Check top right
-//        print "\tTop Right: " . PHP_EOL;
         if ($this->isSymbol($row-1, $column+1)) {
             return true;
         }
 
         // Check right
-//        print "\tRight: " . PHP_EOL;
         if ($this->isSymbol($row, $column+1)) {
             return true;
         }
 
         // Check bottom right
-//        print "\tBottom Right: " . PHP_EOL;
         if($this->isSymbol($row+1, $column+1)) {
             return true;
         }
 
         // Check bottom
-//        print "\tBottom: " . PHP_EOL;
         if ($this->isSymbol($row+1, $column)) {
             return true;
         }
 
         // Check bottom left
-//        print "\tBottom Left: " . PHP_EOL;
         if ($this->isSymbol($row+1, $column-1)) {
             return true;
         }
@@ -110,12 +102,11 @@ class Day3
             return false;
         }
 
-        if ($column < 0 || $column > $this->totalColumns) {
+        if ($column < 0 || $column >= $this->totalColumns) {
             return false;
         }
 
         if (!is_numeric($this->schematic[$row][$column]) && $this->schematic[$row][$column] != '.') {
-            print "\t($row, $column) => " . $this->schematic[$row][$column] . PHP_EOL;
             return true;
         }
         return false;
