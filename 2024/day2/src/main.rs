06872321d75cee2888e4ce9a3c6807475b2b321f
Sean Burke <sean111@gmail.com>
2024-12-02 18:53:35 -0500

    2024-2.2: ✅

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -5,6 +5,8 @@ use std::io::prelude::*;
 fn main() {
     let p1res = part1("data/data.txt").unwrap();
     writeln!(stdout(), "Part1: {}", p1res).unwrap();
+    let p2res = part2("data/data.txt").unwrap();
+    writeln!(stdout(), "Part2: {}", p2res).unwrap();
 }
 
 pub fn part1(file: &str) -> Result<i32> {
@@ -14,7 +16,12 @@ pub fn part1(file: &str) -> Result<i32> {
     let mut safe_count: i32 = 0;
     reader.read_to_string(&mut contents)?;
     for line in contents.lines() {
-        let check: bool = check_line(line);
+        let parts = line.split(" ");
+        let mut data: Vec<i32> = Vec::new();
+        for i in parts {
+            data.push(i.parse::<i32>().unwrap());
+        }
+        let check: bool = check_line(data);
         if check {
             writeln!(stdout(), "Safe Line: {}", line)?;
             safe_count += 1;
@@ -23,15 +30,33 @@ pub fn part1(file: &str) -> Result<i32> {
     Ok(safe_count)
 }
 
-pub fn check_line(line: &str) -> bool {
-    let parts = line.split(" ");
-    // let mut data: Vec<i32> = Vec::new();
+pub fn part2(file: &str) -> Result<i32> {
+    let fp = File::open(file)?;
+    let mut reader = BufReader::new(fp);
+    let mut contents = String::new();
+    let mut safe_count: i32 = 0;
+    reader.read_to_string(&mut contents)?;
+    for line in contents.lines() {
+        let parts = line.split(" ");
+        let mut data: Vec<i32> = Vec::new();
+        for i in parts {
+            data.push(i.parse::<i32>().unwrap());
+        }
+        let check: bool = dampner(data);
+        if check {
+            writeln!(stdout(), "Safe Line: {}", line)?;
+            safe_count += 1;
+        }
+    }
+    Ok(safe_count)
+}
+
+pub fn check_line(line: Vec<i32>) -> bool {
     let mut count: i32 = 0;
     let mut previous: i32 = 0;
     // 1 = up, -1 = down
     let mut direction: i8 = 0;
-    for i in parts {
-        let number = i.trim().parse::<i32>().unwrap();
+    for number in line {
         let mut temp_direction: i8 = 0;
         if count > 0 {
             let temp = number - previous;
@@ -63,6 +88,19 @@ pub fn check_line(line: &str) -> bool {
     true
 }
 
+pub fn dampner(line: Vec<i32>) -> bool {
+    let count = line.len();
+    for i in 0..count {
+        let mut temp_line = line.to_vec();
+        temp_line.remove(i);
+        let check = check_line(temp_line);
+        if check {
+            return true;
+        }
+    }
+    false
+}
+
 #[cfg(test)]
 
 mod tests {
@@ -71,5 +109,10 @@ mod tests {
     fn test_part1() {
         let result = part1("data/test.txt").unwrap();
         assert_eq!(result, 2);
+    }
+    #[test]
+    fn test_part2() {
+        let result = part2("data/test.txt").unwrap();
+        assert_eq!(result, 4);
     }
 }
